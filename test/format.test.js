92d8385a4166253f4c3aed07da2f8ac42127cb21
Kevin Deisz <kevin.deisz@gmail.com>
2020-04-11 16:30:21 -0400

    Prettier write

--- test/format.test.js
+++ test/format.test.js
@@ -3,7 +3,8 @@ const path = require("path");
 const prettier = require("prettier");
 
 const fixture = fs.readFileSync(path.join(__dirname, "./fixture.ini"), "utf-8");
-const format = content => prettier.format(content, { parser: "ini", plugins: ["."] });
+const format = (content) =>
+  prettier.format(content, { parser: "ini", plugins: ["."] });
 
 test("defaults", () => {
   const formatted = format(fixture);
