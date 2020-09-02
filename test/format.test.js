01e66253b19a7bd9d1cd891b73482c8f352f31a8
Adam Averay <adam@averay.com>
2020-09-02 12:10:58 +0900

    Add tests for files without sections

--- test/format.test.js
+++ test/format.test.js
@@ -2,16 +2,24 @@ const fs = require("fs");
 const path = require("path");
 const prettier = require("prettier");
 
-const fixture = fs.readFileSync(path.join(__dirname, "./fixture.ini"), "utf-8");
+const fixtures = ["with-sections", "no-sections"];
 const format = (content, options) =>
   prettier.format(content, { parser: "ini", plugins: ["."], ...options });
 
-test("defaults", () => {
-  const formatted = format(fixture, { iniSpaceAroundEquals: false });
-  expect(formatted).toMatchSnapshot();
-});
+for (const fixtureFile of fixtures) {
+  const fixturePath = path.join(__dirname, "./fixtures/", fixtureFile + '.ini');
+  const fixture = fs.readFileSync(fixturePath, "utf-8");
 
-test("with space around equals", () => {
-  const formatted = format(fixture, { iniSpaceAroundEquals: true });
-  expect(formatted).toMatchSnapshot();
-});
+  describe(fixtureFile + ":", () => {
+    test("defaults", () => {
+      const formatted = format(fixture, { iniSpaceAroundEquals: false });
+      expect(formatted).toMatchSnapshot();
+    });
+
+    test("with space around equals", () => {
+      const formatted = format(fixture, { iniSpaceAroundEquals: true });
+      expect(formatted).toMatchSnapshot();
+    });
+  });
+}
+
