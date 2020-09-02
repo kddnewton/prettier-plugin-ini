fb972bf2f117577df45ab8a0428028dd38ef7f5a
Kevin Deisz <kevin.deisz@gmail.com>
2020-09-02 09:18:02 -0400

    Fix up lint

--- test/format.test.js
+++ test/format.test.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 const path = require("path");
 const prettier = require("prettier");
 
-const fixtures = ["with-sections", "no-sections"];
+const fixtureFiles = ["with-sections", "no-sections"];
 const format = (content, options) =>
   prettier.format(content, { parser: "ini", plugins: ["."], ...options });
 
-for (const fixtureFile of fixtures) {
-  const fixturePath = path.join(__dirname, "./fixtures/", fixtureFile + '.ini');
+fixtureFiles.forEach((fixtureFile) => {
+  const fixturePath = path.join(__dirname, "./fixtures/", `${fixtureFile}.ini`);
   const fixture = fs.readFileSync(fixturePath, "utf-8");
 
-  describe(fixtureFile + ":", () => {
+  describe(`${fixtureFile}:`, () => {
     test("defaults", () => {
       const formatted = format(fixture, { iniSpaceAroundEquals: false });
       expect(formatted).toMatchSnapshot();
@@ -21,5 +21,4 @@ for (const fixtureFile of fixtures) {
       expect(formatted).toMatchSnapshot();
     });
   });
-}
-
+});
