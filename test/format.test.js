fe8c420f79f73c6f8b85a2e6590f95607bd90db5
Kevin Newton <kddnewton@gmail.com>
2022-10-24 11:56:57 -0400

    Updates

--- test/format.test.js
+++ test/format.test.js
@@ -1,30 +1,28 @@
 import { readFileSync } from "fs";
-import prettier from "prettier";
+import { format } from "prettier";
 import plugin from "../src/plugin.js";
 
-const fixtureFiles = ["with-sections", "no-sections"];
-
-function format(content, opts) {
-  return prettier.format(content, {
-    parser: "ini",
-    plugins: [plugin],
-    ...opts
-  });
-}
-
-fixtureFiles.forEach((fixtureFile) => {
+describe.each(["with-sections", "no-sections"])("%s", (fixtureFile) => {
   const fixturePath = new URL(`./fixtures/${fixtureFile}.ini`, import.meta.url);
   const fixture = readFileSync(fixturePath, "utf-8");
 
-  describe(`${fixtureFile}:`, () => {
-    test("defaults", async () => {
-      const formatted = await format(fixture, { iniSpaceAroundEquals: false });
-      expect(formatted).toMatchSnapshot();
+  test("defaults", async () => {
+    const formatted = await format(fixture, {
+      parser: "ini",
+      plugins: [plugin],
+      iniSpaceAroundEquals: false
     });
 
-    test("with space around equals", async () => {
-      const formatted = await format(fixture, { iniSpaceAroundEquals: true });
-      expect(formatted).toMatchSnapshot();
+    expect(formatted).toMatchSnapshot();
+  });
+
+  test("with space around equals", async () => {
+    const formatted = await format(fixture, {
+      parser: "ini",
+      plugins: [plugin],
+      iniSpaceAroundEquals: true
     });
+
+    expect(formatted).toMatchSnapshot();
   });
 });
