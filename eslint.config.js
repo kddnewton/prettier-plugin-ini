23efe0947e7370b6e466904ae3f72238765a755b
Kevin Newton <kddnewton@gmail.com>
2024-04-08 13:41:40 -0400

    Bump eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    ignores: ["bin/languages.js"]
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    }
+  }
+]
